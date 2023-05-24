64ee9857c50684fe5969f906f5510417e777f09c
Neemsigo <Dromorong@gmail.com>
2023-05-24 13:53:11 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt-starter-kuxkb1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nuxt-starter-kuxkb1)
---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    Hey?
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script>
+import W from '~/public/worker.js';
+
+// new W();
+</script>
